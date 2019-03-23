3018e61f7fb0f9df69e81e1510e1a8ce87f33355
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-23 15:58:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,7 +11,7 @@ class Doctor
   end 
   
   def new_appointment(patient, date)
-    appointment = Appointment.new(patient, date, self)
+    appointments = Appointment.new(patient, date, self)
   end 
   
   def self.all 
