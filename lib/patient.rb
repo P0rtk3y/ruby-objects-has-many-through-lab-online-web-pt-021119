b81f0b7bf97549fe2c5a34559c40030f01253241
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-23 15:37:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,17 @@
 class Patient 
   @@all = []
   
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(doctor, self, date)
+  end 
+  
+  def appointments 
+    patient_appointments = Appointments.all.select{|appointment| appointment.patient = self}
+  end 
+  
+  def doctor
+  end 
+  
   def self.all 
     @@all 
   end 
